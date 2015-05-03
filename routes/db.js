b9775bb532d64621e8a4b785cee97aed3dc21d36
Farai Madzima <farai@me.com>
2015-05-03 23:04:00 +0200

    Added poses

--- routes/db.js
+++ routes/db.js
@@ -6,9 +6,9 @@ poses.push({
 				full_name: 'Pranayama', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Mental relaxation',
+								'Detoxification',
+								'Works the nervous, respiratory and circulatory systems'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -22,18 +22,9 @@ poses.push({
 				full_name: 'Ardha Chandrasana with Pada Hastasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Works into the whole skeletal and circulatory systems',
-								'Opens shoulder joints',
-								'Good for frozen shoulder',
-								'Reduces or eliminates pain in the lower back',
-								'Good for abdominal obesity',
-								'Helps with sciatic problems',
-								'Stimulates pituitary gland',
-								'Improves and strengthens every muscle in the central part of the body ',
-								'Tones the spinal nerves and abdominal organs inproving the working of the bowels',
-								'Increases flexibility and strength of rectus abdominis and gluteus maximus',
-								'Exercises colon, pancreas, kidneys, muscular, skeletal, respiratory and glandular systems',
-								'Firms and trims waistline, hips, abdomen, buttocks and thighs'
+								'Helps lower back pain, constipation, obesity of stomach',
+								'Works deltoid, trapezius, pectoralis major and hamstings',
+								'Exercise the colon, pancreas and kidney'
 								], 
 				mind_benefits: [
 								'Alleviates anxiety and reduces mental stress'
@@ -46,9 +37,9 @@ poses.push({
 				full_name: 'Utkatasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Helps to open pelvic area, alleves joint pain and arthritis of the knees',
+								'Improves flexibility in toes and ankles',
+								'Works quadriceps and deltoids'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -63,9 +54,9 @@ poses.push({
 				full_name: 'Eagle pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Strengthens lower extremities',
+								'Increases joint mobility for all 12 major joints',
+								'Control of sexual energies, improves function of central nervous system'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -79,9 +70,9 @@ poses.push({
 				full_name: 'Standing head to knee pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Improves concentration',
+								'Avoids sciatica by improving circulation and flexibility',
+								'Works the muscular, skeletal, reproductive and digestive systems'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -95,9 +86,10 @@ poses.push({
 				full_name: 'Standing bow pulling pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Increases circulation to the heart and lungs',
+								'Improves elasticity of the spine by compressing the spine',
+								'Helps with lower back pain',
+								'Opens diaphram and lungs'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -111,9 +103,10 @@ poses.push({
 				full_name: 'Tulandandasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Increases blood flow all over the body at the same time',
+								'Builds strength in lower extremities',
+								'Works the pancreas, kidney and spleen',
+								'Works muscular, reproductive, nervous, and circulatory system'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -127,9 +120,9 @@ poses.push({
 				full_name: 'Dandayamana Bibhaktapada Paschimottanasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Helps for constipation, abdominal obesity and hyper-acidity',
+								'Increases circulation to the brain and adrenal glands',
+								'Good for small and large intestines'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -143,9 +136,10 @@ poses.push({
 				full_name: 'Triangle pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Good for kidneys',
+								'Helps chemical imbalances in the system',
+								'Helps colitis constipation, hip and back pain, spondilitis',
+								'Improves general mobility and menstrual disorders'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -160,9 +154,9 @@ poses.push({
 				full_name: 'Dandayamana Bibhaktapada Janushirasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Strengthens obliques, iliosoas, quads and deltoids, trapezius',
+								'Helps abdominal obesity, diabetes, thyroid, pancreas and kidneys',
+								'Stimulates endocrine, reproductive, and digestive systems'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -176,9 +170,9 @@ poses.push({
 				full_name: 'Tadasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Creates hip and knee mobility',
+								'Releases abdominal tension',
+								'Helps inflammation of the lower back'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -192,9 +186,10 @@ poses.push({
 				full_name: 'Toe stand', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Creates balance in the body and the mind',
+								'Strengthens stomach muscles',
+								'Helps with arthritis, knee problems and gout pain',
+								'Strengthens weak joints'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -208,9 +203,10 @@ poses.push({
 				full_name: 'Savasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Returns cardiovascular circulation to normal',
+								'Slows heart rate, reduces blood pressure',
+								'Teaches complete relaxation',
+								'Stills and focuses the mind'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -224,9 +220,8 @@ poses.push({
 				full_name: 'Pavanamuktasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Helps constipation, flatulence and hyper acidity',
+								'Massages ascending colon, descending and transverse colon'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -240,9 +235,8 @@ poses.push({
 				full_name: 'Sit-up', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Strengthens and tightens the abdomen',
+								'Stimulates diaphragm and eliminates toxins from lungs'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -257,9 +251,9 @@ poses.push({
 				full_name: 'Cobra pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Works to compress and open up the spine',
+								'Each posture works a specific section of the spine',
+								'Creates spinal changes, helps lower back pain'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -273,9 +267,9 @@ poses.push({
 				full_name: 'Salabhasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Similar to Cobra and more effective in helping slipped disc and sciatica',
+								'Firms buttocks and hips',
+								'Increases spinal strength, flexibility and circulation'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -289,9 +283,9 @@ poses.push({
 				full_name: 'Full locust pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Firms muscles of the abdomen, upper arms, hips and thighs',
+								'Increases spinal strength and flexibility',
+								'Improves flexibility and tone of spinal muscles'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -305,9 +299,9 @@ poses.push({
 				full_name: 'Floor bow pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Increases circulation to heart and lungs, improves oxygen intake',
+								'Opens diaphragm and expands the chest region – improves respiratory conditions',
+								'Opens shoulder joint and helps frozen shoulder conditions'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -321,9 +315,10 @@ poses.push({
 				full_name: 'Supta Vajrasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Helps with sciatica, gout, varicose veins and rheumatism in the legs',
+								'Slims and tones thighs, firms calf muscles and strengthens the abdomen',
+								'Strengthens and improves flexibility of lower spine, knees and ankle joints',
+								'Lubricates and increases circulation to joints'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -337,9 +332,10 @@ poses.push({
 				full_name: 'Half tortoise pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'A rejuvenation pose providing maximum relaxation',
+								'Assists in relieving digestion problems and constipation',
+								'Stretches lower part of the lungs, increases lung capacity',
+								'Excellent for respiratory conditions'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -354,9 +350,10 @@ poses.push({
 				full_name: 'Ustrasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Compresses spine, relieving back problems',
+								'Opens rib cage, lungs and digestive system',
+								'Stimulates nervous system',
+								'Great for lungs and many bronchial problems'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -370,9 +367,11 @@ poses.push({
 				full_name: 'Rabbit pose', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Provides maximum longitudinal extension of the spine',
+								'Stretches the spine to increase nutrition to the nervous system',
+								'Improves the mobility and elasticity of the spine and back muscles',
+								'Stimulates thyroid and parathyroid glands to regulate metabolism',
+								'Balances hormones'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -386,9 +385,11 @@ poses.push({
 				full_name: 'Utkatasana', 
 				description: 'Janushirasana with Paschimottanasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Helps balance blood sugar levels and the metabolism',
+								'Improves flexibility of sciatic nerve and ankle, knee and hip joints',
+								'Enhances kidney function',
+								'Increases circulation to liver, spleen and pancreas',
+								'Stimulates thymus gland, digestion and immune system'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -402,9 +403,10 @@ poses.push({
 				full_name: 'Ardha Matsyendrasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Compresses and stretches spine from the bottom to the top',
+								'Increases hip and back flexibility',
+								'Improves digestion',
+								'Firms the buttocks, thighs and abdomen'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
@@ -418,9 +420,11 @@ poses.push({
 				full_name: 'Kapalbhati in Vajrasana', 
 				description: 'Ardha Chandrasana with Pada Hastasana', 
 				body_benefits: [
-								'Good for lungs and respiratory system',
-								'Helps high blood pressure',
-								'Exercises nervous, respiratory and circulatory systems'
+								'Brings mental clarity',
+								'Strengthens abdominal organs and wall',
+								'Normalises bowels',
+								'Energises body',
+								'Improves oxygenation to the body'
 								], 
 				mind_benefits: [
 								'Helps with mental relaxation',
